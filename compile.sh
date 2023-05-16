d6a22385315d362bf3f7c756ceead501883e18dd
flexy2dd <richard@pochot.com>
2023-05-16 11:25:02 +0200

    Initial commit

---
+++ compile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+git submodule sync
+git submodule init
+git submodule update
+
+pushd external/ntripcaster/ntripcaster0.1.5/
+
+./configure &&
+make &&
+make install
+
+popd
+
+#pushd external/rtklib-demo5/app/str2str/gcc/
+#make
+
+#mkdir -p /usr/local/bin
+#cp str2str /usr/local/bin/
+#popd
+
+pushd external/ntripserver/
+make
+
+#cp ntripserver /usr/local/bin/
+popd
+
